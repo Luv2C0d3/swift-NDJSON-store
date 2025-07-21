ae7f46cb3165cba7fec18376252f1dc2e095d667
Pierre Jacomet <pierrj@hotmail.com>
2025-07-21 13:25:27 -0400

    Updating the system requirement only

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "NDJSONStore",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "NDJSONStore", targets: ["NDJSONStore"])
     ],
